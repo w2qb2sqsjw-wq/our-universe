043f2028a8048eb8a3ec5570c5842096257dac44
w2qb2sqsjw-wq <w2qb2sqsjw@privaterelay.appleid.com>
2026-07-29 17:25:37 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
+window.onload=function(){
 
+setTimeout(function(){
+
+document.getElementById("loading-screen").style.display="none";
+
+document.getElementById("main-content").style.display="flex";
+
+},3000);
+
+};
+
+document.addEventListener("click",function(e){
+
+if(e.target.id==="enterBtn"){
+
+alert("Welcome to your universe, Nanu ❤️");
+
+}
+
+});
