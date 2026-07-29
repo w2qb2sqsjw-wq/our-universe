21452681e4e6a62f77570ed8da26aa1434614e8f
w2qb2sqsjw-wq <w2qb2sqsjw@privaterelay.appleid.com>
2026-07-29 17:35:48 +0500

    Update hearts.js

--- js/hearts.js
+++ js/hearts.js
@@ -1,1 +1,27 @@
+function createHeart(){
 
+    const heart = document.createElement("div");
+
+    heart.className = "heart";
+
+    heart.innerHTML = "❤️";
+
+    heart.style.left = Math.random() * 100 + "vw";
+
+    heart.style.animationDuration = (3 + Math.random() * 5) + "s";
+
+    heart.style.fontSize = (15 + Math.random() * 25) + "px";
+
+    document.body.appendChild(heart);
+
+
+    setTimeout(()=>{
+
+        heart.remove();
+
+    },8000);
+
+}
+
+
+setInterval(createHeart,500);
